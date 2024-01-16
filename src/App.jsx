a95b6df60d341f733a33ecfa188552d239e3f129
Harshana Udayanga <sudayanga1820@gmail.com>
2024-01-17 01:23:50 +0530

    Show all of the categories

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ export default function App() {
     return (
         <>
             <h1>Welcoem to <code>crown clothing</code></h1>
-        
+            <div className="categories-container">
+                <div className="category-container">
+                    {/* Place category image here */}
+                    <div className="category-body-container">
+                        <h2>Hats</h2>
+                        <p>Shop Now</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
