80ab964fe8e91a9fcc8c9c27ed9ead2e20b9469e
Harshana Udayanga <sudayanga1820@gmail.com>
2024-01-17 01:30:18 +0530

    Implment dynamic rendering the categories in the the categories container

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
  
 export default function App() {
+
+    const categories = [
+        {
+            id: 1,
+            title: 'Hats'
+        },
+        {
+            id: 2,
+            title: 'Jackets'
+        },
+        {
+            id: 3,
+            title: 'Sneakers'
+        },
+        {
+            id: 4,
+            title: 'Womens'
+        },
+        {
+            id: 5,
+            title: 'Mens'
+        },
+    ]
+
     return (
         <>
             <h1>Welcoem to <code>crown clothing</code></h1>
             <div className="categories-container">
-                <div className="category-container">
-                    {/* Place category image here */}
-                    <div className="category-body-container">
-                        <h2>Hats</h2>
-                        <p>Shop Now</p>
-                    </div>
-                </div>
+                {
+                    categories.map(({ id, title }) => {
+                        return (
+                            <div className="category-container" key={id}>
+                                {/* Place category image here */}
+                                <div className="category-body-container">
+                                    <h2>{title}</h2>
+                                    <p>Shop Now</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }  
             </div>
         </>
     )
